2814bc239f76a9ba6dde34963741d17a83b056fa
Виктор Траченко <tracenko_va@MacBook-Air-Viktor.local>
2023-02-10 02:01:20 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,9 +1,12 @@
 const timeBlock = document.querySelector('.time');
 const dateBlock = document.querySelector('.date');
+const greetingBlock = document.querySelector('.greeting');
+const nameBlock = document.querySelector('.name');
 
 const showTime = ()=> {
     const date = new Date();
     timeBlock.textContent = date.toLocaleTimeString();
+    showGreeting();
     showDate();
     setTimeout(showTime, 1000);
 }
@@ -19,4 +22,32 @@ const showDate = () => {
     dateBlock.textContent = upperCaseFirst(date.toLocaleDateString('ru-RU', options));
 }
 
+const getTimeOfDay = (hours) => {
+    if (hours < 6) return 'night';
+    if (hours < 12) return 'morning';
+    if (hours < 18) return 'afternoon';
+    return 'evening';
+}
+
+const showGreeting = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const greetingText = `Good ${getTimeOfDay(hours)}`;
+
+    greetingBlock.textContent = greetingText;
+}
+
+
 showTime();
+
+function setLocalStorage() {
+    localStorage.setItem('name', nameBlock.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+        nameBlock.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage)
