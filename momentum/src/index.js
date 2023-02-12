d0402e84f6bb426e85f98f1a550798fdb5e06163
Viktor Trachenko <trachenko.v.a@gmail.com>
2023-02-12 16:56:28 +0300

    feat: add bg + start work with weather

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -4,8 +4,40 @@ const timeBlock = document.querySelector('.time');
 const dateBlock = document.querySelector('.date');
 const greetingBlock = document.querySelector('.greeting');
 const nameBlock = document.querySelector('.name');
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+const bodyBlock = document.body;
 
-const showTime = ()=> {
+const WEATHER_API_KEY='7084f9129f59e9d3a287c7606d73e5ba';
+
+
+
+// ****************** Load and Unload Doc ********************
+
+window.addEventListener('beforeunload', () => {
+    setLocalStorage();
+});
+
+window.addEventListener('load', () => {
+    getLocalStorage();
+});
+
+
+// ****************** Local Storage ********************
+
+const getLocalStorage = () => {
+    if(localStorage.getItem('name')) {
+        nameBlock.value = localStorage.getItem('name');
+    }
+}
+
+const setLocalStorage = () => {
+    localStorage.setItem('name', nameBlock.value);
+}
+
+// ****************** Name Imput Block ********************
+
+const showTime = () => {
     const date = new Date();
     timeBlock.textContent = date.toLocaleTimeString();
     showGreeting();
@@ -23,7 +55,10 @@ const showDate = () => {
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', timeZone: 'Europe/Moscow'};
     dateBlock.textContent = upperCaseFirst(date.toLocaleDateString('ru-RU', options));
 }
-const getTimeOfDay = (hours) => {
+
+const getTimeOfDay = () => {
+    const date = new Date();
+    const hours = date.getHours();
     if (hours < 6) return 'night';
     if (hours < 12) return 'morning';
     if (hours < 18) return 'afternoon';
@@ -31,23 +66,58 @@ const getTimeOfDay = (hours) => {
 }
 
 const showGreeting = () => {
-    const date = new Date();
-    const hours = date.getHours();
-    const greetingText = `Good ${getTimeOfDay(hours)},`;
+    const greetingText = `Good ${getTimeOfDay()},`;
     greetingBlock.textContent = greetingText;
 }
 
-
 showTime();
 
-function setLocalStorage() {
-    localStorage.setItem('name', nameBlock.value);
-  }
-  window.addEventListener('beforeunload', setLocalStorage)
 
-  function getLocalStorage() {
-    if(localStorage.getItem('name')) {
-        nameBlock.value = localStorage.getItem('name');
-    }
+// ****************** BG Image ********************
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+let randomImageNum = getRandomInt(20) + 1;
+
+const setBg = (timeOfDay, bgNum) => {
+    const urlImage=`https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum.toString().padStart(2,'0')}.jpg`;
+    const img = new Image();
+    img.src = urlImage;
+    img.onload = () => {      
+        bodyBlock.style.backgroundImage = `url('${urlImage}')`;
+    }; 
+   
+}
+
+setBg(getTimeOfDay(),randomImageNum);
+
+const getSlideNext = () => {
+    randomImageNum++;
+    if (randomImageNum>20) randomImageNum=1;
+    if (randomImageNum<1) randomImageNum=20;
+    setBg(getTimeOfDay(),randomImageNum);
+}
+
+const getSlidePrev = () => {
+    randomImageNum--;
+    if (randomImageNum<1) randomImageNum=20;
+    setBg(getTimeOfDay(),randomImageNum);
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+
+
+
+// ****************** Weather ********************
+
+async function getWeather(city) {  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${WEATHER_API_KEY}&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+    console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
   }
-  window.addEventListener('load', getLocalStorage)
+
+  //getWeather();
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './momentum/src/index.js',
-  plugins: [
-    new CleanWebpackPlugin(['momentum/dist']),
-  ],
+
   output: {
     path: path.resolve(__dirname, 'momentum/dist'),
     filename: 'bundle.js',
